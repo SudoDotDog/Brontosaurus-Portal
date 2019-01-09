d76fe295d6229b6dce92d8d30791183d9dd98cac
WMXPY <wm@mengw.io>
2019-01-09 13:05:43 -0600

    add error messages

--- src/portal/error.ts
+++ src/portal/error.ts
@@ -11,6 +11,13 @@ const errorMap: Record<string, ErrorInfo> = {
     "Failed to fetch": {
         short: 'Cannot connect to the server',
     },
+    "4001": {
+        short: 'Username - Password not matched',
+        long: 'Connect service',
+    },
+    "4120": {
+        short: 'Internal Error',
+    },
 };
 
 export const wrapMap = (message: string): ErrorInfo => {
