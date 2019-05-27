b62b1715ad0ae02323a38e08b9f8d8363bdb4c40
WMXPY <wm@mengw.io>
2019-05-27 13:31:23 -0500

    reset portal path

--- src/repository/application.ts
+++ src/repository/application.ts
@@ -20,7 +20,7 @@ export const application = async (): Promise<ApplicationRepositoryResponse> => {
         applicationKey: portal.applicationKey,
     });
 
-    const response: Response = await fetch('https://auth.rpngo.com/application', {
+    const response: Response = await fetch('/application', {
         method: "POST",
         headers: {
             'Accept': 'application/json',
