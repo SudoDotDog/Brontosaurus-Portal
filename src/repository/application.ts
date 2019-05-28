0cf937e6575521ffa9a26fe95bbc74ab980ac4f8
WMXPY <wm@mengw.io>
2019-05-28 11:39:41 -0500

    revert temp solution

--- src/repository/application.ts
+++ src/repository/application.ts
@@ -20,12 +20,6 @@ export const application = async (): Promise<ApplicationRepositoryResponse> => {
         applicationKey: portal.applicationKey,
     });
 
-    return {
-        avatar: 'https://www.w3schools.com/w3images/fjords.jpg',
-        background: 'https://sample-videos.com/img/Sample-jpg-image-500kb.jpg',
-        name: 'Hello',
-    };
-
     const response: Response = await fetch('/application', {
         method: "POST",
         headers: {
