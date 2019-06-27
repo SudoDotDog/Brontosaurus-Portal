70a0f3402e598cc4a9e47c5768805b906be41921
WMXPY <wm@mengw.io>
2019-06-27 12:28:23 -0500

    update languages

--- src/i18n/all.ts
+++ src/i18n/all.ts
@@ -15,13 +15,13 @@ export const ENGLISH_UNITED_STATES: Record<PROFILE, string> = {
     [PROFILE.SIGN_IN_TO]: "Sign in to",
     [PROFILE.USERNAME]: "Username",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
+    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with a temporary password, please enter a new password and use it the next time you log in.",
     [PROFILE.NEW_PASSWORD]: "New password",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "Two-factor authentication is enabled on your account, Open the two-factor authentication app on your device to view your authentication code.",
+    [PROFILE.AUTH_CODE]: "Authentication code",
+    [PROFILE.AUTH_AND_SIGN_IN]: "Verify and SignIn",
 };
 
 export const RUSSIAN_RUSSIA: Record<PROFILE, string> = {
@@ -33,13 +33,13 @@ export const RUSSIAN_RUSSIA: Record<PROFILE, string> = {
     [PROFILE.SIGN_IN_TO]: "Войти в",
     [PROFILE.USERNAME]: "Потребителско име",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
-    [PROFILE.NEW_PASSWORD]: "New password",
-    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
+    [PROFILE.LIMBO_DESCRIPTION]: "Вы входите с временным паролем, введите новый пароль и используйте его при следующем входе.",
+    [PROFILE.NEW_PASSWORD]: "Новый пароль",
+    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Сохранить и войти",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "В вашем аккаунте включена двухфакторная аутентификация, Откройте приложение двухфакторной аутентификации на своем устройстве, чтобы просмотреть код аутентификации.",
+    [PROFILE.AUTH_CODE]: "Код аутентификации",
+    [PROFILE.AUTH_AND_SIGN_IN]: "Подтвердите и войдите",
 };
 
 export const FRENCH_FRANCE: Record<PROFILE, string> = {
@@ -51,13 +51,13 @@ export const FRENCH_FRANCE: Record<PROFILE, string> = {
     [PROFILE.SIGN_IN_TO]: "Se connecter à",
     [PROFILE.USERNAME]: "Identifiant",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
-    [PROFILE.NEW_PASSWORD]: "New password",
-    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
+    [PROFILE.LIMBO_DESCRIPTION]: "Vous vous connectez avec un mot de passe temporaire, veuillez entrer un nouveau mot de passe et l'utiliser lors de votre prochaine connexion.",
+    [PROFILE.NEW_PASSWORD]: "Nouveau mot de passe",
+    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Enregistrer et se connecter",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "L’authentification à deux facteurs est activée sur votre compte, Ouvrez l'application d'authentification à deux facteurs sur votre appareil pour afficher votre code d'authentification.",
+    [PROFILE.AUTH_CODE]: "Code d'identification",
+    [PROFILE.AUTH_AND_SIGN_IN]: "3Vérifier et se connecter",
 };
 
 export const JAPANESE_JAPAN: Record<PROFILE, string> = {
@@ -69,13 +69,13 @@ export const JAPANESE_JAPAN: Record<PROFILE, string> = {
     [PROFILE.SIGN_IN_TO]: "にログイン",
     [PROFILE.USERNAME]: "ユーザー名",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
-    [PROFILE.NEW_PASSWORD]: "New password",
-    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
+    [PROFILE.LIMBO_DESCRIPTION]: "あなたは一時的なパスワードでログインしています。新しいパスワードを入力して、次回のログイン時にそれを使用してください。",
+    [PROFILE.NEW_PASSWORD]: "新しいパスワード",
+    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "保存してサインイン",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "アカウントで2要素認証が有効になっている，認証コードを確認するには、デバイスで2要素認証アプリを開きます。",
+    [PROFILE.AUTH_CODE]: "認証コード",
+    [PROFILE.AUTH_AND_SIGN_IN]: "確認してサインイン",
 };
 
 export const KOREAN_KOREA: Record<PROFILE, string> = {
@@ -87,13 +87,13 @@ export const KOREAN_KOREA: Record<PROFILE, string> = {
     [PROFILE.SIGN_IN_TO]: "로그인",
     [PROFILE.USERNAME]: "사용자 아이디",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
-    [PROFILE.NEW_PASSWORD]: "New password",
-    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
+    [PROFILE.LIMBO_DESCRIPTION]: "임시 비밀번호로 로그인하는 중입니다. 다음에 로그인 할 때 새 비밀번호를 입력하여 사용하십시오.",
+    [PROFILE.NEW_PASSWORD]: "새 비밀번호",
+    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "저장 및 로그인",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "귀하의 계정에서 이중 인증이 활성화되어 있고, 기기에서 2 단계 인증 앱을 열어 인증 코드를 확인합니다.",
+    [PROFILE.AUTH_CODE]: "인증 코드",
+    [PROFILE.AUTH_AND_SIGN_IN]: "확인 및 로그인",
 };
 
 export const CHINESE_SIMPLIFIED: Record<PROFILE, string> = {
@@ -105,31 +105,31 @@ export const CHINESE_SIMPLIFIED: Record<PROFILE, string> = {
     [PROFILE.SIGN_IN_TO]: "登录至",
     [PROFILE.USERNAME]: "用户名",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
-    [PROFILE.NEW_PASSWORD]: "New password",
-    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
+    [PROFILE.LIMBO_DESCRIPTION]: "您正在使用临时密码登录，请输入新密码，并在下次登录时使用它。",
+    [PROFILE.NEW_PASSWORD]: "新密码",
+    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "保存并登录",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "您的帐户已启用双因素身份验证，在您的设备上打开双因素身份验证应用以查看您的身份验证代码。",
+    [PROFILE.AUTH_CODE]: "身份验证代码",
+    [PROFILE.AUTH_AND_SIGN_IN]: "验证并登录",
 };
 
 export const CHINESE_TRADITIONAL: Record<PROFILE, string> = {
 
-    [PROFILE.NEED_HELP]: "登錄遇到問題？",
+    [PROFILE.NEED_HELP]: "登入遇到問題？",
     [PROFILE.PASSWORD]: "密鑰",
     [PROFILE.PRIVACY_POLICY]: "隱私政策",
     [PROFILE.SIGN_IN]: "登入",
     [PROFILE.SIGN_IN_TO]: "登入到",
     [PROFILE.USERNAME]: "使用者名稱",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
-    [PROFILE.NEW_PASSWORD]: "New password",
-    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
+    [PROFILE.LIMBO_DESCRIPTION]: "您正在使用臨時密鑰登入，請輸入新密鑰，並在下次登入時使用它。",
+    [PROFILE.NEW_PASSWORD]: "新密鑰",
+    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "保存並登入",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "您的帳戶已啟用雙因素身份驗證，在您的設備上打開雙因素身份驗證應用以查看您的身份驗證代碼。",
+    [PROFILE.AUTH_CODE]: "身份驗證代碼",
+    [PROFILE.AUTH_AND_SIGN_IN]: "驗證並登入",
 };
 
 export const SPANISH_MEXICO: Record<PROFILE, string> = {
@@ -141,11 +141,11 @@ export const SPANISH_MEXICO: Record<PROFILE, string> = {
     [PROFILE.SIGN_IN_TO]: "Iniciar sesión en",
     [PROFILE.USERNAME]: "Usuario",
 
-    [PROFILE.LIMBO_DESCRIPTION]: "You are signing in with temporary password, please input your new password to proceeding",
-    [PROFILE.NEW_PASSWORD]: "New password",
-    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
+    [PROFILE.LIMBO_DESCRIPTION]: "Está iniciando sesión con una contraseña temporal, ingrese una nueva contraseña y úsela la próxima vez que inicie sesión.",
+    [PROFILE.NEW_PASSWORD]: "Nueva contraseña",
+    [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Salvar y Iniciar",
 
-    [PROFILE.TWO_FA_DESCRIPTION]: "1",
-    [PROFILE.AUTH_CODE]: "2",
-    [PROFILE.AUTH_AND_SIGN_IN]: "3",
+    [PROFILE.TWO_FA_DESCRIPTION]: "La autenticación de dos factores está habilitada en su cuenta, Abra la aplicación de autenticación de dos factores en su dispositivo para ver su código de autenticación.",
+    [PROFILE.AUTH_CODE]: "Código de autenticación",
+    [PROFILE.AUTH_AND_SIGN_IN]: "Verificar y Iniciar",
 };
