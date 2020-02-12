494af950494dc7f63106ada4ce309321001907b7
WMXPY <wm@mengw.io>
2020-02-12 15:16:28 -0600

    add reset description

--- src/i18n/all.ts
+++ src/i18n/all.ts
@@ -29,6 +29,8 @@ export const ENGLISH_UNITED_STATES: Record<PROFILE, string> = {
     [PROFILE.NEW_PASSWORD]: "New password",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Save and SignIn",
 
+    [PROFILE.RESET_DESCRIPTION]: "You are signing in with a temporary password, please enter a new password and use it the next time you log in.",
+
     [PROFILE.TWO_FA_DESCRIPTION]: "Two-Factor authentication is enabled on your account, Open the Two-Factor authentication app on your device to view your authentication code.",
     [PROFILE.AUTH_CODE]: "Authentication code",
     [PROFILE.AUTH_AND_SIGN_IN]: "Verify and SignIn",
@@ -87,6 +89,8 @@ export const RUSSIAN_RUSSIA: Record<PROFILE, string> = {
     [PROFILE.RESET_PASSWORD_TEMPORARY_DESCRIPTION]: "Если введенный вами адрес электронной почты и имя пользователя совпадают, вы получите электронное письмо с временным паролем. Если вы не можете найти соответствующий адрес электронной почты, проверьте почтовый ящик для спама или перейдите по ссылке ниже, чтобы посетить наш справочный центр.",
     [PROFILE.NEW_PASSWORD]: "Новый пароль",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Сохранить и войти",
+
+    [PROFILE.RESET_DESCRIPTION]: "Вы входите с временным паролем, введите новый пароль и используйте его при следующем входе.",
 
     [PROFILE.TWO_FA_DESCRIPTION]: "В вашем аккаунте включена двухфакторная аутентификация, Откройте приложение двухфакторной аутентификации на своем устройстве, чтобы просмотреть код аутентификации.",
     [PROFILE.AUTH_CODE]: "Код аутентификации",
@@ -149,6 +153,8 @@ export const FRENCH_FRANCE: Record<PROFILE, string> = {
     [PROFILE.NEW_PASSWORD]: "Nouveau mot de passe",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Enregistrer et se connecter",
 
+    [PROFILE.RESET_DESCRIPTION]: "Vous vous connectez avec un mot de passe temporaire, veuillez entrer un nouveau mot de passe et l'utiliser lors de votre prochaine connexion.",
+
     [PROFILE.TWO_FA_DESCRIPTION]: "L’authentification à deux facteurs est activée sur votre compte, Ouvrez l'application d'authentification à deux facteurs sur votre appareil pour afficher votre code d'authentification.",
     [PROFILE.AUTH_CODE]: "Code d'identification",
     [PROFILE.AUTH_AND_SIGN_IN]: "3Vérifier et se connecter",
@@ -208,6 +214,8 @@ export const JAPANESE_JAPAN: Record<PROFILE, string> = {
     [PROFILE.NEW_PASSWORD]: "新しいパスワード",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "保存してサインイン",
 
+    [PROFILE.RESET_DESCRIPTION]: "あなたは一時的なパスワードでログインしています。新しいパスワードを入力して、次回のログイン時にそれを使用してください。",
+
     [PROFILE.TWO_FA_DESCRIPTION]: "アカウントで2要素認証が有効になっている，認証コードを確認するには、デバイスで2要素認証アプリを開きます。",
     [PROFILE.AUTH_CODE]: "認証コード",
     [PROFILE.AUTH_AND_SIGN_IN]: "確認してサインイン",
@@ -264,6 +272,8 @@ export const KOREAN_KOREA: Record<PROFILE, string> = {
     [PROFILE.RESET_PASSWORD_TEMPORARY_DESCRIPTION]: "입력 한 이메일 주소와 사용자 이름이 일치하면 임시 비밀번호가 포함 된 이메일을 받게됩니다. 해당 이메일을 찾을 수없는 경우 스팸 메일 함을 확인하거나 아래 링크를 클릭하여 도움말 센터를 방문하십시오.",
     [PROFILE.NEW_PASSWORD]: "새 비밀번호",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "저장 및 로그인",
+
+    [PROFILE.RESET_DESCRIPTION]: "임시 비밀번호로 로그인하는 중입니다. 다음에 로그인 할 때 새 비밀번호를 입력하여 사용하십시오.",
 
     [PROFILE.TWO_FA_DESCRIPTION]: "귀하의 계정에서 이중 인증이 활성화되어 있고, 기기에서 2 단계 인증 앱을 열어 인증 코드를 확인합니다.",
     [PROFILE.AUTH_CODE]: "인증 코드",
@@ -322,6 +332,8 @@ export const CHINESE_SIMPLIFIED: Record<PROFILE, string> = {
     [PROFILE.NEW_PASSWORD]: "新密码",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "保存并登录",
 
+    [PROFILE.RESET_DESCRIPTION]: "您正在使用临时密码登录，请输入新密码，并在下次登录时使用它。",
+
     [PROFILE.TWO_FA_DESCRIPTION]: "您的帐户已启用双因素身份验证，在您的设备上打开双因素身份验证应用以查看您的身份验证代码。",
     [PROFILE.AUTH_CODE]: "身份验证代码",
     [PROFILE.AUTH_AND_SIGN_IN]: "验证并登录",
@@ -378,6 +390,8 @@ export const CHINESE_TRADITIONAL: Record<PROFILE, string> = {
     [PROFILE.RESET_PASSWORD_TEMPORARY_DESCRIPTION]: "如果您輸入的電子郵箱地址和用戶名匹配，您將會收到一封包含臨時密鑰的郵件。如果您無法找到對應的郵件，請檢查垃圾郵件信箱，或者點擊下方的鏈接訪問我們的幫助中心。",
     [PROFILE.NEW_PASSWORD]: "新密鑰",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "保存並登入",
+
+    [PROFILE.RESET_DESCRIPTION]: "您正在使用臨時密鑰登入，請輸入新密鑰，並在下次登入時使用它。",
 
     [PROFILE.TWO_FA_DESCRIPTION]: "您的帳戶已啟用雙因素身份驗證，在您的設備上打開雙因素身份驗證應用以查看您的身份驗證代碼。",
     [PROFILE.AUTH_CODE]: "身份驗證代碼",
@@ -437,6 +451,8 @@ export const SPANISH_MEXICO: Record<PROFILE, string> = {
     [PROFILE.RESET_PASSWORD_TEMPORARY_DESCRIPTION]: "Si la dirección de correo electrónico y el nombre de usuario que ingresó coinciden, recibirá un correo electrónico con una contraseña temporal. Si no puede encontrar el correo electrónico correspondiente, verifique su correo no deseado o haga clic en el enlace a continuación para visitar nuestro centro de ayuda.",
     [PROFILE.NEW_PASSWORD]: "Nueva contraseña",
     [PROFILE.SAVE_NEW_PASSWORD_AND_SIGN_IN]: "Salvar y Iniciar",
+
+    [PROFILE.RESET_DESCRIPTION]: "Está iniciando sesión con una contraseña temporal, ingrese una nueva contraseña y úsela la próxima vez que inicie sesión.",
 
     [PROFILE.TWO_FA_DESCRIPTION]: "La autenticación de dos factores está habilitada en su cuenta, Abra la aplicación de autenticación de dos factores en su dispositivo para ver su código de autenticación.",
     [PROFILE.AUTH_CODE]: "Código de autenticación",
