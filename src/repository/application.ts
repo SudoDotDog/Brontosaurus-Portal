5d9cea184fb1f4e59ff01c4624cd6f8dbb1e5ed9
WMXPY <wm@mengw.io>
2020-06-28 15:32:07 -0500

    add peer dependencies

--- src/repository/application.ts
+++ src/repository/application.ts
@@ -5,52 +5,61 @@
  */
 
 import { Portal } from "../portal/portal";
-import { TargetInfo } from "../state/info/type";
+import { TargetInfo, ApplicationRedirection } from "../state/info/type";
 import { joinRoute } from "../util/route";
+import { Fetch } from "@sudoo/fetch";
+
+export type ApplicationRepositoryResponse = {
+
+    readonly avatar?: string;
+    readonly background?: string;
+    readonly help?: string;
+    readonly privacy?: string;
+    readonly favicon?: string;
+
+    readonly name: string;
+    readonly redirections: ApplicationRedirection[];
+    readonly iFrameProtocol: boolean;
+    readonly postProtocol: boolean;
+    readonly alertProtocol: boolean;
+    readonly noneProtocol: boolean;
+
+    readonly systemName?: string;
+    readonly accountName?: string;
+
+    readonly indexPage?: string;
+    readonly entryPage?: string;
+};
 
 export const applicationRepository = async (): Promise<TargetInfo> => {
 
     const portal: Portal = Portal.instance;
 
-    const payload: string = JSON.stringify({
-        applicationKey: portal.applicationKey,
-    });
+    const data: ApplicationRepositoryResponse = await Fetch
+        .post
+        .json(joinRoute('/application'))
+        .add('applicationKey', portal.applicationKey)
+        .fetch();
 
-    const response: Response = await fetch(joinRoute('/application'), {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        mode: "cors",
-        body: payload,
-    });
+    return {
+        timeout: false,
+        avatar: data.avatar,
+        background: data.background,
+        name: data.name,
+        help: data.help,
+        privacy: data.privacy,
+        favicon: data.favicon,
 
-    const data = await response.json();
+        redirections: data.redirections,
+        iFrameProtocol: data.iFrameProtocol,
+        postProtocol: data.postProtocol,
+        alertProtocol: data.alertProtocol,
+        noneProtocol: data.noneProtocol,
 
-    if (response.ok) {
-        return {
-            timeout: false,
-            avatar: data.avatar,
-            background: data.background,
-            name: data.name,
-            help: data.help,
-            privacy: data.privacy,
-            favicon: data.favicon,
+        systemName: data.systemName,
+        accountName: data.accountName,
 
-            redirections: data.redirections,
-            iFrameProtocol: data.iFrameProtocol,
-            postProtocol: data.postProtocol,
-            alertProtocol: data.alertProtocol,
-            noneProtocol: data.noneProtocol,
-
-            systemName: data.systemName,
-            accountName: data.accountName,
-
-            indexPage: data.indexPage,
-            entryPage: data.entryPage,
-        };
-    }
-
-    throw new Error(data);
+        indexPage: data.indexPage,
+        entryPage: data.entryPage,
+    };
 };
