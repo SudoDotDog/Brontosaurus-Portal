e38f81bc0e0ea4c91b546ed60c6058a8e79bf229
WMXPY <wm@mengw.io>
2020-07-27 09:23:51 -0500

    fix dev config

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -4,30 +4,30 @@
  * @description Development
  */
 
-const SudooWebpack = require("@sudoo/webpack-react").SudooWebpack;
-const path = require('path');
+import { SudooWebpackReact } from "@sudoo/webpack-react";
+import * as Path from "path";
 
-const BUILD_DIR = path.resolve(__dirname, '..', 'dist');
-const APP_DIR = path.resolve(__dirname, '..', 'src');
-const TSCONFIG_PATH = path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
-const COMMON_SASS_DIR = path.resolve(__dirname, '..', 'style', 'common');
+const BUILD_DIR: string = Path.resolve(__dirname, '..', 'dist');
+const APP_DIR: string = Path.resolve(__dirname, '..', 'src');
+const TSCONFIG_PATH: string = Path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
+const COMMON_SASS_DIR: string = Path.resolve(__dirname, '..', 'style', 'common');
 
-export default SudooWebpack.create(
-    {
-        APP_DIR,
-        BUILD_DIR,
-        COMMON_SASS_DIR,
+export default SudooWebpackReact.create({
 
-        TSCONFIG_PATH,
+    applicationPath: APP_DIR,
+    applicationEntryFile: 'index.tsx',
 
-        APP_ENTRY_FILE_NAME: 'index.tsx',
+    buildPath: BUILD_DIR,
+    commonSassPath: COMMON_SASS_DIR,
+
+    tsconfigPath: TSCONFIG_PATH,
+}, {
+
+    defines: {
+
+        'process.env.RELEASE_VERSION': JSON.stringify(process.env.RELEASE_VERSION || 'LOCAL'),
+        'process.env.TEST_SERVER_PATH': JSON.stringify(process.env.TEST_SERVER_PATH),
     },
-    {
-        defines: {
-            'process.env.RELEASE_VERSION': JSON.stringify(process.env.RELEASE_VERSION || 'LOCAL'),
-            'process.env.TEST_SERVER_PATH': JSON.stringify(process.env.TEST_SERVER_PATH),
-        },
-        title: 'Brontosaurus',
-        mobile: true,
-    },
-).development(8081);
+    title: 'Brontosaurus',
+    mobile: true,
+}).development(8081);
